05017d5502a291251ba22d698d2f2e02e407ebff
Joshua Ohlman <codewriterohs@gmail.com>
2014-11-05 14:13:26 -0600

    Publish version v1.0.0

--- package.js
+++ package.js
@@ -1,12 +1,17 @@
 Package.describe({
-  summary: "Exposes the users collection as Users and provides collection and template helpers for the users collection."
+  summary: "Exposes the users collection as Users and provides collection and template helpers."
+  , name: 'cwohlman:users-collection'
+  , version: "1.0.0"
+  , git: 'https://github.com/cwohlman/meteor-users-collection.git'
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR@1.0");
+
   api.use('templating');
   api.use('accounts-base');
   api.use('underscore');
-  
+
   api.add_files('users/both/collections/users.js', ['client', 'server']);
   api.add_files('users/client/helpers.js', 'client');
   api.add_files('users/server/collections/users.js', 'server');

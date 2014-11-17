508bd994c2d07221fe043dcf3be1e7c522b8bc1e
Joshua Ohlman <codewriterohs@gmail.com>
2014-11-17 16:46:02 -0600

    Don't allow users to edit roles, services or emails

--- users/server/collections/users.js
+++ users/server/collections/users.js
@@ -1,11 +1,25 @@
 // If you want to modify the rights on user updates
 // then add a new allow rule in your app.
 
+
+// this code allows editing any field except roles and services
+
 Users.allow({
-	// doesn't allow insert or removal of users from untrusted code
+  // this allow rule is modified by the deny rule below to prevent users from
+  // setting roles or services.
     update: function (userId, doc, fieldNames, modifier) {
-        return Users.isAdmin(userId) 
-        		// only admins can update user roles via the client
-        		|| (doc._id === userId && !_.contains(fieldNames, 'roles'));
+        return (doc._id === userId || Users.isAdmin(userId));
     }
+});
+
+// prevents non-admins from setting roles, services or emails.
+// all three of these contain security information which the user shouldn't be
+// allowed to set.
+// this can't be overridden in your app (and you shouldn't).
+Users.deny({
+  update: function (userId, doc, fieldNames) {
+    console.log(_.union(fieldNames, ['roles', 'services', 'emails']));
+    return !Users.isAdmin(userId) &&
+    _.union(fieldNames, ['roles', 'services', 'emails']).length;
+  }
 });
